8eaa3a026367666ba099fba955f2a42dc3e2bdad
tommyd450 <tommyd811@gmail.com>
2021-11-07 14:10:41 +0000

    Refactor: Template list page; HTTP API calls centralized.

---
+++ src/api/tmdb-api.js
@@ -0,0 +1,32 @@
+
+export const getMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDBKEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then(res => res.json())
+      .then(json => json.results);
+  };
+  
+  export const getMovie = id => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDBKEY}`
+    ).then(res => res.json());
+  };
+  
+  export const getGenres = () => {
+    return fetch(
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+        process.env.REACT_APP_TMDBKEY +
+        "&language=en-US"
+    )
+      .then(res => res.json())
+      .then(json => json.genres);
+  };
+  
+  export const getMovieImages = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDBKEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => json.posters);
+  };
